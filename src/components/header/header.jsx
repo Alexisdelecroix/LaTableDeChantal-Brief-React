9b65e318f1242566d98b0bb4600aaf95c3e11c70
Delecroix <d-alexis@hotmail.fr>
2023-01-31 10:23:58 +0100

    w3c

---
+++ src/components/header/header.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+
+
+
+export default function header() {
+
+
+        return (
+                <header>
+                    <div>
+                    <p>La table de Chantal</p>
+                    </div>
+
+                    <ul>
+                        <li>
+                            
+                        </li>
+                    </ul>
+
+
+
+
+
+
+                </header>
+
+
+        )
+
+
+}
