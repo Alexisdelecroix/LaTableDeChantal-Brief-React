e1d37e3974c12f001ec52cc0454906238fad4827
Delecroix <d-alexis@hotmail.fr>
2023-01-31 11:21:15 +0100

    ajout photo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Monheader from './components/header/Header';
+import MyPresentation from './components/presentation/Presentation';
 
 function App() {
   return (
     <div className="App">
      <Monheader/>
+     <MyPresentation/>
     </div>
   );
 }
--- src/components/presentation/presentation.jsx
+++ src/components/presentation/presentation.jsx
@@ -7,17 +7,42 @@ export default function Presentation() {
 
 
     return (
-        <div>
+        <article>
             <div>
             <img></img>
             </div>
 
+            <div>
+                <h1>Le restaurant</h1>
+
+                <p>Lorem ipsum dolor sit amet consectetur, 
+                    adipisicing elit. Necessitatibus natus consectetur
+                     corporis magnam nobis et, nihil alias, provident tempora 
+                     numquam eligendi accusantium rem vel. Delectus nesciunt 
+                     labore nemo cumque ducimus?</p>
+            </div>
 
 
+            <div>
+            <img></img>
+            </div>
+
+            <div>
+                <h1>Le chef</h1>
+
+                <p>Lorem ipsum dolor sit amet consectetur, 
+                    adipisicing elit. Necessitatibus natus consectetur
+                     corporis magnam nobis et, nihil alias, provident tempora 
+                     numquam eligendi accusantium rem vel. Delectus nesciunt 
+                     labore nemo cumque ducimus?</p>
+            </div>
+
+        </article>
+        
+
+        
 
 
-
-        </div>
 
 
     )
