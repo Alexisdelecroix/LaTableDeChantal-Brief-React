4fb077eed68324de755813526746679dc05bdb03
Julian Brasseur <brasseurjulian2@gmail.com>
2023-01-31 12:22:34 +0100

    Ajout logo Footer

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Facebook from "../../images/facebook.svg"
+
+export default function footer() {
+    return (
+        <article>
+            <div>
+                <img src={Facebook} alt="" />
+            </div>
+        </article>
+    )
+}
