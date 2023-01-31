fafed168ab972fc7ef2d57e8f07f81e0ac80b3b8
Delecroix <d-alexis@hotmail.fr>
2023-01-31 16:54:48 +0100

    Test rooter

---
+++ src/components/formulaire/formulaire.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MyHeader from '../header/Header';
+
+
+export default function Plat() {
+
+    return (
+    
+        <div>
+        <h1>dfsdsf</h1>
+        <h2>fdssf</h2>
+        </div>
+    )
+}
--- src/pages/Plat.jsx
+++ src/pages/Plat.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import Myheader from '../components/header/Header'
 import MyPlat from '../components/cartePlat/Plat'
+import MyFooter from "../components/footer/Footer";
 
 export default function Plat() {
 
     
         return (
             <div>
+                
 <Myheader/>
 <MyPlat/>
+<MyFooter/>
             </div>
         );
     }
